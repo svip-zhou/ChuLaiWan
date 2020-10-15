ce754c2d74c6e53b138a93e0bc72551bc6a0c4a5
JoeMiller <1615655751@qq.com>
2020-10-15 23:43:43 +0800

    第一次使用git提交文件

---
+++ jar/FristApplication/src/main/java/com/ChuLaiWanGroup/FristApplication/FristApplication.java
@@ -0,0 +1,13 @@
+package com.ChuLaiWanGroup.FristApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FristApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FristApplication.class, args);
+	}
+
+}
---
+++ jar/FristApplication/src/test/java/com/ChuLaiWanGroup/FristApplication/FristApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ChuLaiWanGroup.FristApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FristApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
